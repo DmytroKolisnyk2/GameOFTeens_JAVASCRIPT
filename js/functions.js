89a71bc2dae1296dfca1e412959a52f206a87e4d
MaxPopsuy <maim09p@gmail.com>
2021-06-26 17:25:17 +0300

    submit btn js

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+const answersArray = ["", "", "", "", "", "", "", "", "", ""];
+let questionIndex = 0;
+export const addAnswer = function () {
+	const inputRadios = document.querySelectorAll('.question__form--variant');
+
+	for (let i = 0, length = inputRadios.length; i < length; i++) {
+	if (inputRadios[i].checked) {
+		// answersArray.push(inputRadios[i].value);
+		localStorage.setItem("index", questionIndex);
+		answersArray.splice(localStorage.getItem("index"), 1, inputRadios[i].value);
+		localStorage.setItem("answers", answersArray);
+		alert(answersArray);
+		questionIndex++;
+		break;
+	}
+	}
+}
+
+document.querySelector('.question__submit').addEventListener('click', addAnswer);
